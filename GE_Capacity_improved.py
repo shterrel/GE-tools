35987c9975592ccfcec72ee8b951d430010e5bab
Squirrel <shterrel@gmail.com>
2018-09-21 11:52:09 -0500

    updated

--- GE_Capacity_improved.py
+++ GE_Capacity_improved.py
@@ -2,28 +2,26 @@
 #capacity email that is generated weekly for GE
 #built by Squirrel
 
+def percentage(a,b):
+    results = ((a / b) * 100)
+    return "Percentage: " + "%.2f" % results + "%"
+
 print ("What is memory_mb_used and memory_mb?")
 memory_mb_used = float(input("memory_mb_used: "))
 memory_mb = float(input("memory_mb: "))
-answer_memory = ((memory_mb_used / memory_mb) * 100)
-Memory = ("%.2f" % answer_memory)
-print "Memory Percentage: " + "%.2f" % answer_memory + "%"
+memory = percentage(memory_mb_used,memory_mb)
 
 print ("What is vcpus_used and vcpus?")
 vcpus_used = float(input('vcpus_used: '))
 vcpus = float(input('vcpus: ') * 4)
-answer_vcpu = ((vcpus_used / vcpus) * 100)
-VCPU = ("%.2f" % answer_vcpu)
-print "VCPUs Percentage: " + "%.2f" % answer_vcpu + "%"
+vcpu = percentage(vcpus_used, vcpus)
 
 print ("What is disk_avalable_least and local_gb?")
 disk_available_least = float(input("disk_available_least: "))
 local_gb = float(input("local_gb: "))
-answer_disk = ((disk_available_least / local_gb) * 100)
-Disk = ("%.2f" % answer_disk)
-print "Disk Percentage: " + "%.2f" % answer_disk + "%"
+disk = percentage(disk_available_least,local_gb)
 
 print "RECAP: "
-print "RAM Utlization : " + Memory + "%"
-print "VCPU Utlization: " + VCPU + "%"
-print "DISK Utlization: " + Disk + "%"
+print "Memory" + str(memory)
+print "VCPU" + str(vcpu)
+print "Disk" + str (disk)
